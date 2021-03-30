22816876efe143938106e8b68657a9a55b2fee22
Nicosen <SUCCESSW2M@163.com>
2021-03-30 15:52:10 +0800

    eject

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import {Button} from 'antd'
 
 function App() {
-  return (
-    <div className="App" id='app'>
-          Edit src/App.js and save to reload.
-    </div>
-  );
+    return (
+        <div className="App header">
+            <Button>测试</Button>
+            Edit src/App.js and save to reload.
+        </div>
+    );
 }
 
 export default App;
