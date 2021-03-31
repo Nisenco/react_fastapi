5de4972a885f7008f5411a1202cfe40ab0df28d1
Nicosen <SUCCESSW2M@163.com>
2021-03-31 15:26:00 +0800

    '添加页面布局'

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,10 +1,13 @@
-import React,{useState} from 'react'
+import React, {useState} from 'react'
+import LayoutIndex from '../layout'
 import './home.scss'
 
-export default function Home(props){
+export default function Home(props) {
 
 
     return <div className="home-container">
-        <h1>Home</h1>
+        <LayoutIndex>
+            <h1>Home</h1>
+        </LayoutIndex>
     </div>
 }
